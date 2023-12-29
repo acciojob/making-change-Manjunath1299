5f4ac379422386bebed26707a72c22ad53fa4f50
Manjunath <66263712+Manjunath1299@users.noreply.github.com>
2023-12-29 14:01:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 const makeChange = (c) => {
   // your name here
+	const coinValues = {
+    q: 25,
+    d: 10,
+    n: 5,
+    p: 1
+  };
+
+  const change = {};
+
+  for (const [coin, value] of Object.entries(coinValues)) {
+    change[coin] = Math.floor(c / value);
+    c %= value;
+  }
+
+  return change;
 };
 
 // Do not the change the code below
+const calculateChange = () => {
+  const changeInput = document.getElementById("change-input");
+  const resultElement = document.getElementById("result");
+
+  const c = parseInt(changeInput.value);
+  const changeObj = makeChange(c);
+
+  resultElement.textContent = `Change: ${JSON.stringify(changeObj)}`;
+};
+
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
